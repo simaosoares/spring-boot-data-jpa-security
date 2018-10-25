b6dc74785ee5bc452d829cb8af050f496f387af8
Simão Soares <s.soares@moliporex.pt>
2018-10-25 14:25:44 +0100

    Initial commit.

---
+++ src/main/java/com/example/spring_boot_data_jpa_security/SpringBootDataJpaSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring_boot_data_jpa_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaSecurityApplication.class, args);
+	}
+}
